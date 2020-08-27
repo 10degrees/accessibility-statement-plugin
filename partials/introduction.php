2418150557b14db942b1cf066da275b7e9abfc23
Ralph Morris <ralph@10degrees.uk>
2020-08-27 13:47:21 +0100

    typo

--- partials/introduction.php
+++ partials/introduction.php
@@ -1,3 +1,3 @@
-<h1><?php _e('Accessibilty Statement for', 'a11y-statement');?> <?php echo $website_name; ?></h1>
+<h1><?php _e('Accessibility Statement for', 'a11y-statement');?> <?php echo $website_name; ?></h1>
 
 <p><?php echo $organisation; ?> <?php _e('is commited to ensuring digital accessibility for people with disabilities. We are continually improving the user experience for everyone, and applying the relevant accessibility standards.', 'a11y-statement');?></p>
