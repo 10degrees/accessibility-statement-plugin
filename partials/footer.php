bee9594b34fdcf41b9afe8386d3d7ca86544d996
Alice Williams <alice@10degrees.uk>
2020-09-08 10:37:42 +0100

    Fix error in the footer.

--- partials/footer.php
+++ partials/footer.php
@@ -4,4 +4,4 @@
  * TODO: hyperlink Accessibility Statement Generator to Github page, when exists.
  */
 ?>
-<p><?php _('This statement was created on', 'a11y-statement');?> <?php echo $date_of_publication; ?> <?php _e('using the Accessibility Statement Generator Plugin', 'a11y-statement'); ?>. </p>
+<p><?php _e('This statement was created on', 'a11y-statement');?> <?php echo $date_of_publication; ?> <?php _e('using the Accessibility Statement Generator Plugin', 'a11y-statement'); ?>. </p>
