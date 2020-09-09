0cc2d694d870bb1ca285fd85c41197513b84474d
Alice Williams <alice@10degrees.uk>
2020-09-09 14:48:30 +0100

    Refactor section url into variable for readability. Use esc_html_e on section title.

--- settings-page.php
+++ settings-page.php
@@ -9,9 +9,14 @@ $active_tab = isset( $_GET['tab'] ) ? $_GET['tab'] : $sections[0]->get_id();  ?>
 	<h1><?php _e( 'Accessibility Statement', 'a11y-statement' ); ?></h1>
 
 	<h2 class="nav-tab-wrapper">
-		<?php foreach ( $sections as $section ) : ?>
-			<a href="<?php echo esc_url( admin_url( 'options-general.php?page=accessibility-statement&tab=' . $section->get_id() ) ); ?>" class="nav-tab<?php echo $active_tab === $section->get_id() ? ' nav-tab-active' : ''; ?>"><?php echo $section->get_title(); ?></a>
-		<?php endforeach; ?>
+		<?php
+		foreach ( $sections as $section ) : 
+			$section_url = admin_url( 'options-general.php?page=accessibility-statement&tab=' . $section->get_id() );
+			?>
+			<a href="<?php echo esc_url($section_url); ?>" class="nav-tab<?php echo $active_tab === $section->get_id() ? ' nav-tab-active' : ''; ?>"><?php esc_html_e($section->get_title()); ?></a>
+		<?php 
+		endforeach; 
+		?>
 	</h2>
 
 	<form action="options.php" method="POST" class="main-settings">
