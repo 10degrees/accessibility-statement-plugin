d49df27178ba14cbeabe346b2022ab347ba6fec3
Alice Williams <alice@10degrees.uk>
2020-09-14 13:59:46 +0100

    Move actions into the plugin class so that we can redirect.

--- class-accessibility-statement-plugin.php
+++ class-accessibility-statement-plugin.php
@@ -22,7 +22,86 @@ class AccessibilityStatementPlugin {
 	 */
 	public function __construct() {
 		add_action( 'admin_menu', array( $this, 'add_settings_page' ) );
-		add_action( 'display_post_states', array( $this, 'add_post_state' ), 10, 2)  ;
+		add_action( 'display_post_states', array( $this, 'add_post_state' ), 10, 2);
+		add_action( 'admin_notices', array( $this, 'add_admin_notices' ) );
+
+		add_action( 'admin_post_set_accessibility_statement', array( $this, 'set_accessibility_statement' ) );
+		add_action( 'admin_post_create_accessibility_statement', array($this, 'create_accessibility_statement') );
+	}
+
+	public function create_accessibility_statement() {
+		$default_accessibility_statement_content = StatementGenerator::get_default_content();
+
+		$accessibility_statement_id = wp_insert_post(
+			array(
+				'post_title' => __( 'Accessibility Statement' ),
+				'post_status' => 'draft',
+				'post_type' => 'page',
+				'post_content' => $default_accessibility_statement_content,
+			),
+			true,
+		);
+
+		if( is_wp_error( $accessibility_statement_id ) ) {
+			wp_redirect( admin_url( 'options-general.php?page=accessibility-statement&page-created-error=1' ) );
+			exit;
+		} else {
+			update_option( 'wp_page_for_accessibility_statement' , $accessibility_statement_id);
+
+			wp_redirect( admin_url( 'post.php?post=' . $accessibility_statement_id . '&action=edit' ) );
+			exit;
+		}
+	}
+
+	public function set_accessibility_statement() {
+		$accessibility_page_id = isset($_POST['page_for_accessibility_statement']) ? (int) $_POST['page_for_accessibility_statement'] : 0;
+
+		update_option( 'wp_page_for_accessibility_statement', $accessibility_page_id );
+
+		wp_redirect( admin_url( 'options-general.php?page=accessibility-statement&page-updated=1' ) );
+		exit;
+	}
+
+	/**
+	 * Display notices on settings page
+	 *
+	 * @return void
+	 */
+	public function add_admin_notices() {
+		$screen = get_current_screen();
+
+		if ( 'settings_page_accessibility-statement' === $screen->id ) {
+			if ($_GET['page-updated']) {
+				$accessibility_statement_updated_message = __( 'Accessibility Statement page updated successfully.' );
+
+				$accessibility_page_id = get_option('wp_page_for_accessibility_statement');
+
+				if ($accessibility_page_id) {
+					if (
+						'publish' === get_post_status( $accessibility_page_id )
+						&& current_user_can( 'edit_theme_options' )
+						&& current_theme_supports( 'menus' )
+					) {
+						$accessibility_statement_updated_message = sprintf(
+							/* translators: %s: URL to Customizer -> Menus. */
+							__( 'Accessibility Statement page setting updated successfully. Remember to <a href="%s">update your menus</a>!' ),
+							esc_url( add_query_arg( 'autofocus[panel]', 'nav_menus', admin_url( 'customize.php' ) ) )
+						);
+					}
+				}
+
+				add_settings_error( 'page_for_accessibility_statement', 'page_for_accessibility_statement', $accessibility_statement_updated_message, 'success' );
+			}
+
+			if($_GET['page-created-error']) {
+				add_settings_error(
+					'page_for_accessibility_statement',
+					'page_for_accessibility_statement',
+					__( 'Unable to create an Accessibility Statement.' ),
+					'error',
+				);
+			}
+		}
 	}
 
 	/**
--- settings-page.php
+++ settings-page.php
@@ -1,68 +1,9 @@
 <?php
 /**
- * Admin Page
- */
-
+* Admin Page
+*/
 if ( ! current_user_can( 'manage_privacy_options' ) ) {
 	wp_die( __( 'Sorry, you are not allowed to manage the accessibility statement on this site.' ) );
-}
-
-$action = isset( $_POST['action'] ) ? $_POST['action'] : '';
-
-if( ! empty( $action ) ) {
-	check_admin_referer( $action );
-
-	if( 'set-accessibility-statement' === $action) {
-		$accessibility_page_id = isset($_POST['page_for_accessibility_statement']) ? (int) $_POST['page_for_accessibility_statement'] : 0;
-
-		update_option( 'wp_page_for_accessibility_statement', $accessibility_page_id );
-
-		$accessibility_statement_updated_message = __( 'Accessibility Statement page updated successfully.' );
-
-		if ($accessibility_page_id) {
-			if (
-				'publish' === get_post_status( $accessibility_page_id )
-				&& current_user_can( 'edit_theme_options' )
-				&& current_theme_supports( 'menus' )
-			) {
-				$accessibility_statement_updated_message = sprintf(
-					/* translators: %s: URL to Customizer -> Menus. */
-					__( 'Accessibility Statement page setting updated successfully. Remember to <a href="%s">update your menus</a>!' ),
-					esc_url( add_query_arg( 'autofocus[panel]', 'nav_menus', admin_url( 'customize.php' ) ) )
-				);
-			}
-		} 
-
-		add_settings_error( 'page_for_accessibility_statement', 'page_for_accessibility_statement', $accessibility_statement_updated_message, 'success' );
-
-	} elseif( 'create-accessibility-statement' === $action ) {
-
-		$default_accessibility_statement_content = StatementGenerator::get_default_content();
-
-		$accessibility_statement_id = wp_insert_post(
-			array(
-				'post_title' => __( 'Accessibility Statement' ),
-				'post_status' => 'draft',
-				'post_type' => 'page',
-				'post_content' => $default_accessibility_statement_content,
-			),
-			true,
-		);
-
-		if( is_wp_error( $accessibility_statement_id ) ) {
-			add_settings_error(
-				'page_for_accessibility_statement',
-				'page_for_accessibility_statement',
-				__( 'Unable to create an Accessibility Statement.' ),
-				'error',
-			);
-		} else {
-			update_option( 'wp_page_for_accessibility_statement' , $accessibility_statement_id);
-
-			// wp_redirect( admin_url( 'post.php?post=' . $accessibility_statement_id . '&action=edit' ) );
-			// exit;
-		}
-	}
 }
 
 $accessibility_statement_exists = false;
@@ -124,8 +65,8 @@ if ( ! empty( $accessibility_statement_id ) ) {
 
 				if ( $has_pages ): 
 					?>
-					<form action="" method="POST" class="main-settings">
-						<input type="hidden" name="action" value="set-accessibility-statement">
+					<form action="admin-post.php" method="POST" class="main-settings">
+						<input type="hidden" name="action" value="set_accessibility_statement">
 						<?php
 						wp_dropdown_pages(
 							array(
@@ -137,15 +78,15 @@ if ( ! empty( $accessibility_statement_id ) ) {
 							)
 						);
 
-						wp_nonce_field( 'set-accessibility-statement' );
+						wp_nonce_field( 'set_accessibility_statement' );
 
 						submit_button( __( 'Use This Page' ), 'primary', 'submit', false, array( 'id' => 'set-page' ) );
 						?>
 					</form>
 				<?php endif; ?>
 
-				<form action="" method="POST">
-					<input type="hidden" name="action" value="create-accessibility-statement">
+				<form action="admin-post.php" method="POST">
+					<input type="hidden" name="action" value="create_accessibility_statement">
 					<span>
 						<?php
 						if( $has_pages ) {
@@ -157,7 +98,7 @@ if ( ! empty( $accessibility_statement_id ) ) {
 					</span>
 
 					<?php
-					wp_nonce_field( 'create-accessibility-statement' );
+					wp_nonce_field( 'create_accessibility_statement' );
 
 					submit_button( __( 'Create New Page' ), 'primary', 'submit', false, array( 'id' => 'create-page' ) );
 					?>
