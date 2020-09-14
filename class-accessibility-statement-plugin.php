d8cdbf34df8341db2ed7095811f1496779c1e555
Alice Williams <alice@10degrees.uk>
2020-09-14 14:14:06 +0100

    Move notices into functions.

--- class-accessibility-statement-plugin.php
+++ class-accessibility-statement-plugin.php
@@ -71,36 +71,74 @@ class AccessibilityStatementPlugin {
 		$screen = get_current_screen();
 
 		if ( 'settings_page_accessibility-statement' === $screen->id ) {
-			if ($_GET['page-updated']) {
-				$accessibility_statement_updated_message = __( 'Accessibility Statement page updated successfully.' );
+			$this->get_update_notices();
+			$this->get_creation_notices();
 
-				$accessibility_page_id = get_option('wp_page_for_accessibility_statement');
+			$this->get_selected_statement_notices();
+		}
+	}
 
-				if ($accessibility_page_id) {
-					if (
-						'publish' === get_post_status( $accessibility_page_id )
-						&& current_user_can( 'edit_theme_options' )
-						&& current_theme_supports( 'menus' )
-					) {
-						$accessibility_statement_updated_message = sprintf(
-							/* translators: %s: URL to Customizer -> Menus. */
-							__( 'Accessibility Statement page setting updated successfully. Remember to <a href="%s">update your menus</a>!' ),
-							esc_url( add_query_arg( 'autofocus[panel]', 'nav_menus', admin_url( 'customize.php' ) ) )
-						);
-					}
-				}
+	private function get_selected_statement_notices() {	
+		$accessibility_statement_id = (int) get_option( 'wp_page_for_accessibility_statement' );
 
-				add_settings_error( 'page_for_accessibility_statement', 'page_for_accessibility_statement', $accessibility_statement_updated_message, 'success' );
-			}
-
-			if($_GET['page-created-error']) {
+		if ( ! empty( $accessibility_statement_id ) ) {
+			$accessibility_statement = get_post( $accessibility_statement_id );
+			if ( ! $accessibility_statement instanceof WP_Post ) {
 				add_settings_error(
 					'page_for_accessibility_statement',
 					'page_for_accessibility_statement',
-					__( 'Unable to create an Accessibility Statement.' ),
+					__( 'The currently selected Accessibility Statement page does not exist. Please create or select a new page.' ),
 					'error',
 				);
+			} else {
+				if ( 'trash' === $accessibility_statement->post_status ) {
+					add_settings_error(
+						'page_for_accessibility_statement',
+						'page_for_accessibility_statement',
+						sprintf(
+							/* translators: %s: URL to Pages Trash. */
+							__( 'The currently selected Accessibility Statement page is in the Trash. Please create or select a new Accessibility Statement page or <a href="%s">restore the current page</a>.' ),
+							'edit.php?post_status=trash&post_type=page'
+						),
+						'error'
+					);
+				}
 			}
+		}
+	}
+
+	private function get_creation_notices() {
+		if($_GET['page-created-error']) {
+			add_settings_error(
+				'page_for_accessibility_statement',
+				'page_for_accessibility_statement',
+				__( 'Unable to create an Accessibility Statement.' ),
+				'error',
+			);
+		}
+	}
+
+	private function get_update_notices() {
+		if ($_GET['page-updated']) {
+			$accessibility_statement_updated_message = __( 'Accessibility Statement page updated successfully.' );
+
+			$accessibility_page_id = get_option('wp_page_for_accessibility_statement');
+
+			if ($accessibility_page_id) {
+				if (
+					'publish' === get_post_status( $accessibility_page_id )
+					&& current_user_can( 'edit_theme_options' )
+					&& current_theme_supports( 'menus' )
+				) {
+					$accessibility_statement_updated_message = sprintf(
+						/* translators: %s: URL to Customizer -> Menus. */
+						__( 'Accessibility Statement page setting updated successfully. Remember to <a href="%s">update your menus</a>!' ),
+						esc_url( add_query_arg( 'autofocus[panel]', 'nav_menus', admin_url( 'customize.php' ) ) )
+					);
+				}
+			}
+
+			add_settings_error( 'page_for_accessibility_statement', 'page_for_accessibility_statement', $accessibility_statement_updated_message, 'success' );
 		}
 	}
 
--- settings-page.php
+++ settings-page.php
@@ -6,35 +6,7 @@ if ( ! current_user_can( 'manage_privacy_options' ) ) {
 	wp_die( __( 'Sorry, you are not allowed to manage the accessibility statement on this site.' ) );
 }
 
-$accessibility_statement_exists = false;
 $accessibility_statement_id = (int) get_option( 'wp_page_for_accessibility_statement' );
-
-if ( ! empty( $accessibility_statement_id ) ) {
-	$accessibility_statement = get_post( $accessibility_statement_id );
-	if ( ! $accessibility_statement instanceof WP_Post ) {
-		add_settings_error(
-			'page_for_accessibility_statement',
-			'page_for_accessibility_statement',
-			__( 'The currently selected Accessibility Statement page does not exist. Please create or select a new page.' ),
-			'error',
-		);
-	} else {
-		if ( 'trash' === $accessibility_statement->post_status ) {
-			add_settings_error(
-				'page_for_accessibility_statement',
-				'page_for_accessibility_statement',
-				sprintf(
-					/* translators: %s: URL to Pages Trash. */
-					__( 'The currently selected Accessibility Statement page is in the Trash. Please create or select a new Accessibility Statement page or <a href="%s">restore the current page</a>.' ),
-					'edit.php?post_status=trash&post_type=page'
-				),
-				'error'
-			);
-		} else {
-			$privacy_policy_page_exists = true;
-		}
-	}
-}
 ?>
 
 <div class="wrap">
