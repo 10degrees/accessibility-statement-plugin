510832727ca6456537d02053d10f09176810d663
Alice Williams <alice@10degrees.uk>
2020-09-14 14:43:44 +0100

    Remove settings link.

--- accessibility-statement-generator.php
+++ accessibility-statement-generator.php
@@ -11,17 +11,3 @@ require_once 'helpers.php';
 require_once 'class-accessibility-statement-plugin.php';
 
 new AccessibilityStatementPlugin();
-
-add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'add_settings_link' );
-
-/**
- * Add a Settings link on the plugins page
- *
- * @param   array $links  Array of links.
- *
- * @return  array          Array of links
- */
-function add_settings_link( $links ) {
-	$links[] = '<a href="' . admin_url( 'options-general.php?page=accessibility-statement' ) . '">' . __( 'Settings', 'a11y-statement' ) . '</a>';
-	return $links;
-}
